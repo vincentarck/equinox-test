707b1512f26d459b37c1a4a038c709177a091e44
Vincent laurensius <vincenthambaya@gmail.com>
2023-10-24 18:38:43 +0700

    [FIX] Delete Cars API

--- app/api/cars/[carId]/route.ts
+++ app/api/cars/[carId]/route.ts
@@ -40,39 +40,18 @@ export async function PATCH(
   }
 }
 
-export async function PATCH(
+export async function DELETE(
   req: Request,
   { params }: { params: { carId: string } }
 ) {
   try {
-    const { car_name, day_rate, image_url, month_rate } = await req.json();
-
-    if(!car_name){
-      return new NextResponse("Car name missing", {status: 401});
-    }
-    if(!day_rate){
-      return new NextResponse("Day rate missing", {status: 401});
-    }
-    if(!image_url){
-      return new NextResponse("Image url missing", { status: 401 });
-    }
-    if(!month_rate){
-      return new NextResponse("Month rate missing", { status: 401 });
-    }
-    
-    const updatedCar = await db.car.update({
+    const deletedCar = await db.car.delete({
       where: {
         id: Number(params.carId),
       },
-      data: {
-        car_name,
-        day_rate,
-        image_url,
-        month_rate,
-      }
     });
 
-    return NextResponse.json(updatedCar);
+    return NextResponse.json(deletedCar);
   } catch (error) {
     console.log("[SERVER_ID_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
