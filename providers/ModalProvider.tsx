310b1ca1397a10583a65a2cda22a1f313b56915c
Vincent laurensius <vincenthambaya@gmail.com>
2023-10-24 21:14:27 +0700

    [FEAT] State management and schema form provider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,10 @@
+import EditCarsModal from '@/components/modals/EditCarsModal'
+import React from 'react'
+
+export default function ModalProvider() {
+  return (
+    <div>
+      <EditCarsModal />
+    </div>
+  )
+}
